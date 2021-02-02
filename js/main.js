4dd7a98de9a97c29b803315d9fd44e34dacb6227
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2021-02-02 15:44:57 +0200

    Upload File
    Upload File

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.icon-aside').click(function () {
+    $('.aside').toggleClass('hide-aside');
+    $('.dash-board').toggleClass('dash-board-fullScreen');
+    $(this).find('i').toggleClass('fa-align-left');
+    $(this).find('i').toggleClass('fa-align-right');
+  });
+  $('.aside li').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    var DataValue = $(this).attr('data-value');
+    //-$('.operation').addClass('show').siblings().not('.filter-container').slideUp(500).removeClass('show');
+    $('#content').load(DataValue);
+    console.log(DataValue);
+    
+  });
+});
